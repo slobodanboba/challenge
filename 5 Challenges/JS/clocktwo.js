e1cd89328e6279eaac85e66a3a43180b9f8ba6a1
Boba <slobodan@agavelab.com>
2018-04-02 22:28:46 -0500

    2aprendhome

--- 5 Challenges/JS/clocktwo.js
+++ 5 Challenges/JS/clocktwo.js
@@ -7,9 +7,8 @@ let citiesList = [];
 let theCity= [];
 let cityOption = '';
 let selectedOption = false;
-let selectOption = document.querySelectorAll('[name=options]');
+let selectedCityOption = document.querySelector('.selectedSelect');
 const inputCity = document.querySelector("#form");
-const selectedCityOption = document.querySelector(".selectedSelect");
 
 
 function getConfirmation(e) {
@@ -26,58 +25,78 @@ fetch('https://raw.githubusercontent.com/slobodanboba/challenge/master/5%20Chall
      return `<option name='options' value=${city.name} data-country=${city.country}>${city.name} ${city.country}</option>`
     ;
   }).join('');
-  selectOption = document.querySelectorAll('[name=options]');
-  console.log(selectOption);
 });
 }
 
 inputCity.addEventListener("submit", getConfirmation);
 
 function getLaLonSelected(e) {
-    theCity.filter(city => city.country == e.target.dataset.country)
-    .then(city => {
-      latInputCity = city.coord.lat;
-      console.log('latInputCity',latInputCity);
-      lonInputCity = city.coord.lon;
-      selectedOption = true;
-    })
+  console.log("heyhay");
 }
+//   selectedOption = true;
+//   let selectedCity = this.value;
+//   console.log(selectedCity);
+//   theCity.filter(city => city.name == `${selectedCity}` && city.country == e.dataset.country)
+//   .then(city => {
+//     latSelected = city.coord.lat;
+//     lonSelected = city.coord.lon;
+//   })
+//   .then(function() {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latSelected}&lon=${lonSelected}&units=metric&APPID=261e313010ab3d43b1344ab9eba64cfa`)
+//     .then(response => response.json())
+//     .then(function(data) {
+//       wheatherTokyo = data.main.temp ;
+//       document.querySelector(".tempTokyo").innerHTML = `${Math.round(wheatherTokyo)}`;
+//       let wheatherTokyoF = (wheatherTokyo * 1.8)+32;
+//       document.querySelector(".tempFTokyo").innerHTML = `${Math.round(wheatherTokyoF)}`;
+//       wheatherIconTokyo = data.weather[0].icon;
+//       document.querySelector(".icon-Tokyo").innerHTML = `<img class="icon-Img-Tokyo" src="../content/${wheatherIconTokyo}.png" width="70px" height="70px">`;
+//     })
+//     .then(function(){
+//       fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latSelected},${lonSelected}&timestamp=1331161200&key=AIzaSyANpHwd0ZvP_2qrvqEEp-5l6NS3LkwxSbY `)
+//       .then(response => response.json())
+//       .then(function(selectedCity) {
+//         offsetSelectedCity = selectedCity.rawOffset
+//       });
+//     });
+//   });
+// };
 
 
-selectOption.forEach(option => option.addEventListener('click', getLaLonSelected));
+document.querySelector('.selectedSelect').childNodes.forEach(option => option.addEventListener('click', getLaLonSelected));
 
-function getValue(e) {
-  if(selectedOption) {
-  e.preventDefault();
-  inputValue = (this.querySelector('[name=inputCity]')).value
-  getConfirmation()
-  fetch(` https://maps.googleapis.com/maps/api/geocode/json?address=${inputValue}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
-.then(response => response.json())
-  .then(function(inputCity) {
-    latInputCity = inputCity.results[0].geometry.location.lat;
-    lonInputCity = inputCity.results[0].geometry.location.lng;
-  })
-  .then(function(){
-    fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latInputCity},${lonInputCity}&timestamp=1331161200&key=AIzaSyANpHwd0ZvP_2qrvqEEp-5l6NS3LkwxSbY `)
-    .then(response => response.json())
-    .then(function(inputCity) {
-      offsetinputCity = inputCity.rawOffset
-    })
-    .then(function(){
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latInputCity}&lon=${lonInputCity}&units=metric&APPID=261e313010ab3d43b1344ab9eba64cfa`)
-      .then(response => response.json())
-      .then(function(data) {
-        wheatherTokyo = data.main.temp ;
-        document.querySelector(".tempTokyo").innerHTML = `${Math.round(wheatherTokyo)}`;
-        let wheatherTokyoF = (wheatherTokyo * 1.8)+32;
-        document.querySelector(".tempFTokyo").innerHTML = `${Math.round(wheatherTokyoF)}`;
-        wheatherIconTokyo = data.weather[0].icon;
-        document.querySelector(".icon-Tokyo").innerHTML = `<img class="icon-Img-Tokyo" src="../content/${wheatherIconTokyo}.png" width="70px" height="70px">`;
-      });
-    });
-  });
-};
-}
+// function getValue(e) {
+//   if(selectedOption) {
+//   e.preventDefault();
+//   inputValue = (this.querySelector('[name=inputCity]')).value
+//   getConfirmation()
+//   fetch(` https://maps.googleapis.com/maps/api/geocode/json?address=${inputValue}&key=AIzaSyAhbhZNE6A-Zcg49SMCyO7r_lH4MCDylRc `)
+// .then(response => response.json())
+//   .then(function(inputCity) {
+//     latInputCity = inputCity.results[0].geometry.location.lat;
+//     lonInputCity = inputCity.results[0].geometry.location.lng;
+//   })
+//   .then(function(){
+//     fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latInputCity},${lonInputCity}&timestamp=1331161200&key=AIzaSyANpHwd0ZvP_2qrvqEEp-5l6NS3LkwxSbY `)
+//     .then(response => response.json())
+//     .then(function(inputCity) {
+//       offsetinputCity = inputCity.rawOffset
+//     })
+//     .then(function(){
+//       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latInputCity}&lon=${lonInputCity}&units=metric&APPID=261e313010ab3d43b1344ab9eba64cfa`)
+//       .then(response => response.json())
+//       .then(function(data) {
+//         wheatherTokyo = data.main.temp ;
+//         document.querySelector(".tempTokyo").innerHTML = `${Math.round(wheatherTokyo)}`;
+//         let wheatherTokyoF = (wheatherTokyo * 1.8)+32;
+//         document.querySelector(".tempFTokyo").innerHTML = `${Math.round(wheatherTokyoF)}`;
+//         wheatherIconTokyo = data.weather[0].icon;
+//         document.querySelector(".icon-Tokyo").innerHTML = `<img class="icon-Img-Tokyo" src="../content/${wheatherIconTokyo}.png" width="70px" height="70px">`;
+//       });
+//     });
+//   });
+// };
+// }
 
 // selectOption.forEach(option => option.addEventListener('click', getValue));
 
